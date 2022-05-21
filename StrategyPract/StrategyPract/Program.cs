abdc05799dc8cac3165fe679d6720f5974601dc3
{name?] <82699322+tsplk@users.noreply.github.com>
2022-05-21 14:48:56 +0800

    Практика по стратегии

---
+++ StrategyPract/StrategyPract/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StrategyPract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Keyboard MyKeyboard = new Keyboard(68, new MechanicalSystem());
+            MyKeyboard.Print();
+            MyKeyboard.PrintSystem = new MembraneSystem();
+            MyKeyboard.Print();
+        }
+    }
+        interface IPrintable
+        {
+            void Print();
+        }
+
+        class MechanicalSystem : IPrintable
+        {
+            public void Print()
+            {
+                Console.WriteLine("Клац-клац, я механическая клавиатура!");
+            }
+        }
+
+        class MembraneSystem : IPrintable
+        {
+            public void Print()
+            {
+                Console.WriteLine("Плюх-плюх, я мембранная клавиатура!");
+            }
+        }
+
+        class Keyboard
+        {
+            public IPrintable PrintSystem { private get; set; }
+            private int _numberOfKeys;
+            
+
+            public Keyboard(int num, IPrintable sys)
+            {
+                PrintSystem = sys;
+                _numberOfKeys = num;
+            }
+
+            public void Print()
+            {
+                PrintSystem.Print();
+            }
+        }
+
+    
+}
